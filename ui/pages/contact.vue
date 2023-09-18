a9be97fcc6707c3cae3a186d1e31469e42694381
anshulbanwala <80454613+anshulbanwala@users.noreply.github.com>
2023-09-19 01:55:40 +0530

    Pics are added

--- ui/pages/contact.vue
+++ ui/pages/contact.vue
@@ -87,23 +87,23 @@
             <v-col md="7" cols="12">
               <div class="text-h4 font-weight-black mb-8">Contact Us</div>
               <p class="mb-10">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Get in touch with us today! Our dedicated team is here to assist you with any questions, concerns, or inquiries you may have. We value your feedback and look forward to helping you plan your next adventure. Reach out and let's start the conversation!
               </p>
               <contactForm />
             </v-col>
           </v-row>
         </v-container>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.285388316138!2d-81.37969424867693!3d28.531140482371708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77b041135b063%3A0x13aab64f9cc768fc!2s1400%20Main%20Ln%2C%20Orlando%2C%20FL%2032801%2C%20USA!5e0!3m2!1sen!2sin!4v1606230494596!5m2!1sen!2sin"
-          width="100%"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3428.344938705505!2d76.78419437511405!3d30.76489688407829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ff282845c404b:0xc0844bf7cbdcc6e9!2sPunjab%20Engineering%20College!5e0!3m2!1sen!2sin!4v1695065409591!5m2!1sen!2sin"
           height="600"
+          width="100%"
           frameborder="0"
           style="border: 0; display: block"
           allowfullscreen="true"
           aria-hidden="false"
           tabindex="0"
         ></iframe>
+        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3428.344938705505!2d76.78419437511405!3d30.76489688407829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ff282845c404b%3A0xc0844bf7cbdcc6e9!2sPunjab%20Engineering%20College!5e0!3m2!1sen!2sin!4v1695065409591!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
       </v-col>
     </v-row>
   </section>
@@ -121,8 +121,8 @@ export default {
         {
           src: "pexels-andrea-piacquadio-3830745.jpg",
           heading: " Contact Us ",
-          subHeading: "The most affordable pricing",
-          description: "lorem contact",
+          subHeading: "Get in Touch with Us",
+          description: "Get in touch with us today! Our dedicated team is here to assist you with any questions, concerns, or inquiries you may have. We value your feedback and look forward to helping you plan your next adventure. Reach out and let's start the conversation!",
         },
       ],
     };
