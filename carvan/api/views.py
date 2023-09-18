70cd07e05e4217b4e6a67c8e4c0069ea4497b76f
Adhiyyan <akapoolia@thesummitgrp.com>
2023-09-18 22:07:03 +0530

    backend

--- carvan/api/views.py
+++ carvan/api/views.py
@@ -10,6 +10,12 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from carvan.settings import BASE_DIR
+import json
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 # Create your views here.
@@ -22,21 +28,8 @@ class PlacesViewSet(viewsets.ModelViewSet):
         queryset = Places.objects.all()
         my_type = self.request.query_params.get('type', None)
 
-        order_list = ["Chandigarh", "Delhi"]  # Replace with your list of objects
-        order_dict = {obj: index for index, obj in enumerate(order_list)}
-
         if my_type is not None:
             queryset = queryset.filter(type=my_type)
-        
-        #     queryset = queryset.annotate(
-        #     custom_order=Case(
-        #         *[When(id=key, then=Value(value)) for key, value in order_dict.items()],
-        #         default=Value(len(order_list)),
-        #         output_field=IntegerField()
-        #     )
-        # )
-        queryset = sorted(queryset, key=lambda obj: order_list.index(obj.name))
-        # queryset = queryset.order_by('custom_order')
 
         return queryset
 
@@ -51,12 +44,28 @@ class VisitingViewSet(viewsets.ModelViewSet):
     queryset = Visiting.objects.all()
     serializer_class = VisitingDataSerializer
 
+    def get_queryset(self):
+        queryset = Visiting.objects.all()
+        my_type = self.request.query_params.get('type', None)
+
+        order_list = model()
+
+        if my_type is not None:
+            queryset = queryset.filter(type=my_type)
+
+        queryset = sorted(queryset, key=lambda obj: order_list.index(obj.name))
+
+        return queryset
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
+        print(request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 @api_view(['POST'])
 def signup(request):
@@ -80,3 +89,55 @@ def login_view(request):
             return JsonResponse({'message': 'Login successful'})
         else:
             return JsonResponse({'error': 'Invalid credentials'}, status=400)
+        
+def model():
+    # Load the dataset
+    with open(BASE_DIR/"carvan"/"data"/"places_dataset.json", 'r') as file:
+        data = json.load(file)
+
+    # Extract features (rating, visits_per_month, ticket_price, hours_open, family_friendly)
+    features = np.array([
+        [place["rating"], place["visits_per_month"], place["ticket_price"], place["hours_open"], int(place["family_friendly"])]
+        for place in data["places"]
+    ])
+
+    # Normalize the features using StandardScaler
+    scaler = StandardScaler()
+    normalized_features = scaler.fit_transform(features)
+
+    # Create target labels (ranking based on features)
+    rankings = np.argsort(np.argsort(-normalized_features.sum(axis=1)))
+
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(normalized_features, rankings, test_size=0.2, random_state=42)
+
+    # Build a neural network model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(X_train.shape[1],)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(1, activation='linear')  # Linear activation for regression
+    ])
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    # Train the model
+    model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, verbose=1)
+
+    # Evaluate the model on the test data
+    loss = model.evaluate(X_test, y_test, verbose=0)
+    print(f"Test Loss: {loss:.4f}")
+
+    # Make recommendations based on model predictions
+    recommendation_scores = model.predict(normalized_features).flatten()
+    sorted_indices = np.argsort(recommendation_scores)[::-1]
+    recommended_places = [data["places"][i]["name"] for i in sorted_indices]
+
+    result=[]
+    # Print the recommended places
+    for i, place_name in enumerate(recommended_places):
+        result.append(place_name)
+    
+    return result
---
+++ carvan/script.py
@@ -0,0 +1,34 @@
+import os
+import django
+from django.conf import settings
+
+# Set the DJANGO_SETTINGS_MODULE environment variable.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carvan.settings")
+
+# Initialize Django.
+django.setup()
+
+from api.models import Places,Visiting
+from carvan.settings import BASE_DIR
+import json
+
+
+def manual_post():
+    with open(BASE_DIR/"carvan"/"data"/"places_dataset.json", 'r') as file:
+        data = json.load(file)
+    for d in data["places"]:
+        a = Visiting.objects.get_or_create(
+            city = Places.objects.first(),
+            name = d["name"],
+                                rating = d["rating"],
+                                visits_per_month =d["visits_per_month"],
+                                ticket_price=d["ticket_price"],
+                                hours_open =d["hours_open"],
+                                accessibility_range=d["accessibility_range"],
+                                location=d["location"],
+                                description=d["description"],
+                                highlight =d["highlight"],
+                                family_friendly =d["family_friendly"],
+                                ) 
+
+manual_post()
