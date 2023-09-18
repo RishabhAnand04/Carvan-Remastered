140e25a90e3bf17ec9f4de0d8225df92db2d56fb
Adhiyyan <akapoolia@thesummitgrp.com>
2023-09-18 23:09:36 +0530

    cors

--- carvan/carvan/settings.py
+++ carvan/carvan/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
+    'corsheaders',
     'api',
     'rest_framework',
 ]
@@ -127,7 +127,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:8081"
-]
+# CORS_ALLOWED_ORIGINS =['*']
+ALLOWED_HOSTS=['*']
