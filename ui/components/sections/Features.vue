c91202f6c3600a77f38fdde383c38e782a8e8979
sahilsummit <sjaura@thesummitgrp.com>
2023-09-18 20:55:56 +0530

    minor

--- ui/components/sections/Features.vue
+++ ui/components/sections/Features.vue
@@ -3,14 +3,9 @@
     <v-container>
       <v-row>
         <v-col class="text-center">
-          <h2 class="text-h4 text-md-h3 text-center font-weight-black">
-            Build your website with this Theme
+          <h2 class="text-h4 text-md-h3 text-center font-weight-black mb-2">
+            Our top visited destinations
           </h2>
-          <div
-            class="text-h5 text-md-h4 text-center font-weight-black py-3 mb-10"
-          >
-            Purchase now. Only $49!
-          </div>
         </v-col>
       </v-row>
       <v-row>
@@ -21,12 +16,14 @@
           sm="4"
           class="text-center"
         >
-          <v-avatar
-            size="80"
-            class="display-1 white font-weight-bold primary--text mb-5"
-            style="opacity: 1.15"
-            >{{ card.callout }}</v-avatar
-          >
+          <v-avatar size="128" class="mb-7">
+              <img
+                :src="`${card.img}`"
+                alt="John"
+                width="128"
+                height="128"
+              />
+            </v-avatar>
           <div class="title text-uppercase mt-1 mb-4" v-text="card.title"></div>
 
           <p v-text="card.text"></p>
@@ -34,11 +31,6 @@
             <v-col cols="12"> </v-col>
           </v-row>
         </v-col>
-      </v-row>
-      <v-row no-gutters>
-        <v-btn x-large rounded color="white primary--text mx-auto mt-10 px-16">
-          <v-icon dark left> mdi-check-bold </v-icon>Buy It Now
-        </v-btn>
       </v-row>
     </v-container>
   </section>
@@ -50,22 +42,25 @@ export default {
     return {
       cards: [
         {
-          title: 'Material Design',
+          title: 'Chandigarh',
           subtitle: 'Best Productivity',
-          text: 'Similique sunt in culpa qui officia deserunt mollitia animi, id est laborut dolorum fuga.harum quidem rerum facilis estexpedita distinctio.',
+          text: 'Chandigarh, known for its meticulously planned architecture and lush green gardens, offers tourists a serene blend of urban sophistication and natural beauty.',
           callout: '01',
+          img: 'carousel4.jpg'
         },
         {
-          title: 'Affordable Prices',
+          title: 'Shimla',
           subtitle: 'Special Offers',
-          text: 'Similique sunt in culpa qui officia deserunt mollitia animi, id est laborut dolorum fuga.harum quidem rerum facilis estexpedita distinctio.',
+          text: 'Shimla, nestled in the Himalayas, is a captivating hill station known for its scenic beauty, colonial architecture, and a serene escape for tourists seeking cool climate and breathtaking vistas.',
           callout: '02',
+          img: 'carousel4.jpg'
         },
         {
-          title: 'Fast Loading',
+          title: 'Manali',
           subtitle: 'Income Flow',
-          text: 'Similique sunt in culpa qui officia deserunt mollitia animi, id est laborut dolorum fuga.harum quidem rerum facilis estexpedita distinctio.',
+          text: 'Manali, nestled in the Himalayas, is a popular tourist destination known for its stunning landscapes, adventurous activities, and vibrant culture.',
           callout: '03',
+          img: 'carousel4.jpg'
         },
       ],
     }
